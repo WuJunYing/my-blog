1742193372a01ac8bad327af7c249e4eefd10e7c
WuJunYing <493856393@qq.com>
2018-12-07 23:14:39 +0800

    首页数据获取

--- src/main/java/com/xiaowuu/MyBlogApplication.java
+++ src/main/java/com/xiaowuu/MyBlogApplication.java
@@ -2,14 +2,18 @@ package com.xiaowuu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.xiaowuu.*"})
-public class MyBlogApplication extends WebMvcConfigurationSupport {
+public class MyBlogApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyBlogApplication.class);
+	}
 
 	/**
 	 * 1、 extends WebMvcConfigurationSupport
@@ -17,11 +21,13 @@ public class MyBlogApplication extends WebMvcConfigurationSupport {
 	 * setUseSuffixPatternMatch : 设置是否是后缀模式匹配，如“/user”是否匹配/user.*，默认真即匹配；
 	 * setUseTrailingSlashMatch : 设置是否自动后缀路径模式匹配，如“/user”是否匹配“/user/”，默认真即匹配；
 	 */
-	@Override
-	public void configurePathMatch(PathMatchConfigurer configurer) {
-		configurer.setUseSuffixPatternMatch(true);
-		configurer.setUseTrailingSlashMatch(false);
-	}
+//	@Override
+//	public void configurePathMatch(PathMatchConfigurer configurer) {
+//		configurer.setUseSuffixPatternMatch(true);
+//		configurer.setUseTrailingSlashMatch(false);
+//	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBlogApplication.class, args);
